9b2ab57ab1ac22f2f5d6257dce88f6d3c20d653d
zhangjinzhe <zhangjinzhe@88.com>
2022-05-13 17:13:35 +0800

    feat: 获取storage为空问题

--- src/sources/getStorage.js
+++ src/sources/getStorage.js
@@ -1,5 +1,6 @@
 export const getStorage = (key) => {
   const item = localStorage.getItem(key) || sessionStorage.getItem(key);
+  if (!item) return null;
   const origin = JSON.parse(item);
   if (origin.expire && (origin.expire <= new Date().getTime())) {
     (origin.mode === 'session' ? sessionStorage : localStorage).removeItem(key);
