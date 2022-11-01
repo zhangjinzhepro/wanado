cb9946bcb5b1d29ad1e4c9b3a06314778fa4177e
张晋哲 <zhangjinzhe@88.com>
2022-11-01 16:57:26 +0800

    fix: 千分位问题

--- src/sources/toThousands.js
+++ src/sources/toThousands.js
@@ -3,4 +3,8 @@
  * @param target
  * @returns {string}
  */
-export const toThousands = (target) => String(target).replace(/(\d)(?=(\d{3})+$)/g, ($1) => `${$1},`);
+export const toThousands = (target) => {
+  const ghost = String(target);
+  const float = ghost.includes('.');
+  return float ? `${ghost.split('.')[0].replace(/(\d)(?=(\d{3})+$)/g, ($1) => `${$1},`)}.${ghost.split('.')[1]}` : ghost.replace(/(\d)(?=(\d{3})+$)/g, ($1) => `${$1},`);
+};
