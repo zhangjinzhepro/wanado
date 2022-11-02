3ced4b350d420522c516fbb949822e354b545745
张晋哲 <zhangjinzhe@88.com>
2022-11-02 16:49:19 +0800

    fix: 完善代码

--- test/checkType.test.js
+++ test/checkType.test.js
@@ -1,1 +1,11 @@
-export const checkType = (value) => Object.prototype.toString.call(value).replace(/^\[(\w+)\s(\w+)]$/, (str, $1, $2) => $2);
+import { checkType } from '../src/sources/checkType';
+
+test('检查类型', () => {
+  expect(checkType(1)).toBe('Number');
+  expect(checkType('123123')).toBe('String');
+  expect(checkType([1, 2, 3])).toBe('Array');
+  expect(checkType({ a: 1 })).toBe('Object');
+  expect(checkType(undefined)).toBe('Undefined');
+  expect(checkType(null)).toBe('Null');
+  expect(checkType(() => {})).toBe('Function');
+});
