3d54acaa833020ad211fabf3e87d1b03275c0a37
张晋哲 <zhangjinzhe@88.com>
2022-11-07 09:39:28 +0800

    feat: 2.2.0

--- test/objectCopy.test.js
+++ test/objectCopy.test.js
@@ -1,6 +1,39 @@
 import { objectCopy } from '../src/sources/objectCopy';
 
-test('对象克隆', () => {
-  expect(objectCopy({ a: 1, b: 2, c: { aa: 11, bb: 22 } })).toStrictEqual({ a: 1, b: 2, c: { aa: 11, bb: 22 } });
-  expect(objectCopy({ a: 1, b: 2, c: { aa: 11, bb: 22 } }, 'deep')).toStrictEqual({ a: 1, b: 2, c: { aa: 11, bb: 22 } });
-});
+test(
+  '对象克隆',
+  () => {
+    expect(objectCopy({
+      a: 1,
+      b: 2,
+      c: {
+        aa: 11,
+        bb: 22,
+      },
+    }))
+      .toStrictEqual({
+        a: 1,
+        b: 2,
+        c: {
+          aa: 11,
+          bb: 22,
+        },
+      });
+    expect(objectCopy({
+      a: 1,
+      b: 2,
+      c: {
+        aa: 11,
+        bb: 22,
+      },
+    }, 'deep'))
+      .toStrictEqual({
+        a: 1,
+        b: 2,
+        c: {
+          aa: 11,
+          bb: 22,
+        },
+      });
+  },
+);
